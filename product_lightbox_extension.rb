2d5010ff936ec310c30d104a09e472e48ff0ac90
Roman Smirnov <POMAHC@gmail.com>
2009-08-17 12:37:32 +0400

    Delete first image from thumnails to avoid duplication, but leave it in lightbox gallery.

--- product_lightbox_extension.rb
+++ product_lightbox_extension.rb
@@ -4,5 +4,8 @@ class ProductLightboxExtension < Spree::Extension
   url "http://spreehq.org"
   def activate
     # admin.tabs.add "Product Lightbox", "/admin/product_lightbox", :after => "Layouts", :visibility => [:all]
+    Spree::BaseController.class_eval do
+       helper ProductImageHelper
+    end
   end
 end
